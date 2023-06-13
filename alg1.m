d844821b4f1d659860599ddab4df33a4021a1e74
Kacper Szczepanowski <kacperszcz159@gmail.com>
2023-06-13 21:04:25 +0200

    first code version

---
+++ alg1.m
@@ -0,0 +1,32 @@
+%f1=figure; f2=figure;
+
+z=1;
+Kd=10000;
+Kp=100000;
+K1=1;
+K0=1;
+
+Theta1=3.1;
+Theta2=9.5;
+Theta3=0.24;
+Theta4=0.77;
+g=9.81;
+  [t]=sim('p1sim'); 
+  figure(1); hold on; grid on;
+ % subplot(1,2,1);hold on; grid on;
+  %[t]=sim('p1sim');
+%[t2]=sim('p1sim1');
+title('zmiana wartości e_1 w czasie');
+xlabel('czas[s]')
+ylabel('e_1(t)[rad]')
+ plot(t,e1,'r');
+ figure(2); hold on; grid on;
+%subplot(1,2,2);hold on; grid on;
+
+title('zmiana wartości e_2 w czasie');
+xlabel('czas[s]');
+ylabel('e_2(t)[rad]');
+ plot(t, e2, 'b');
+
+
+  
